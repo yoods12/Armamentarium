e275f0eff4b749b14d2565f86ef2e411a0e16716
유동수 <yoods12@naver.com>
2024-08-13 21:41:27 +0900

    ..
    ..

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -4,12 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CarController : MonoBehaviour
-{
-    private float horizontalInput, verticalInput;
-    private float currentSteerAngle, currentbreakForce;
-    private bool isBreaking;
-
-    // Settings
+{    // Settings
     [SerializeField] private float motorForce, breakForce, maxSteerAngle;
 
     // Wheel Colliders
@@ -20,6 +15,18 @@ public class CarController : MonoBehaviour
     [SerializeField] private Transform frontLeftWheelTransform, frontRightWheelTransform;
     [SerializeField] private Transform rearLeftWheelTransform, rearRightWheelTransform;
 
+    private float horizontalInput, verticalInput;
+    private float currentSteerAngle, currentbreakForce;
+    private bool isBreaking;
+    public Rigidbody rb;
+    Transform playerTransform;
+
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.centerOfMass = Vector3.zero;
+    }
     private void FixedUpdate()
     {
         GetInput();
@@ -31,10 +38,10 @@ public class CarController : MonoBehaviour
     private void GetInput()
     {
         // Steering Input
-        horizontalInput = Input.GetAxis("Horizontal");
+        horizontalInput = Input.GetAxisRaw("Horizontal");
 
         // Acceleration Input
-        verticalInput = Input.GetAxis("Vertical");
+        verticalInput = Input.GetAxisRaw("Vertical");
 
         // Breaking Input
         isBreaking = Input.GetKey(KeyCode.Space);
@@ -75,8 +82,9 @@ public class CarController : MonoBehaviour
     {
         Vector3 pos;
         Quaternion rot;
-        wheelCollider.GetWorldPose(out pos, out rot);
-        wheelTransform.rotation = rot;
-        wheelTransform.position = pos;
+        wheelCollider.GetWorldPose(out pos, out rot); 
+
+        wheelTransform.position = Vector3.Lerp(wheelTransform.position, pos, Time.deltaTime * 10f); // 10f는 보간 속도
+        wheelTransform.rotation = Quaternion.Slerp(wheelTransform.rotation, rot, Time.deltaTime * 10f);
     }
 }
---
+++ Assets/Scripts/FollowCam.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCam : MonoBehaviour
+{
+    public Transform target;
+    public float moveDamping = 15f;
+    public float rotateDamping = 10f;
+    public float distance = 5f;
+    public float height = 4f;
+    public float targetOffset = 2f;
+
+    Transform tr;
+
+    void Start()
+    {
+        tr = GetComponent<Transform>();
+    }
+    void LateUpdate()
+    {
+        var camPos = target.position - (target.forward * distance) + (target.up * height);
+        tr.position = Vector3.Slerp(tr.position, camPos, moveDamping * Time.deltaTime);
+        tr.rotation = Quaternion.Slerp(tr.rotation, target.rotation, rotateDamping * Time.deltaTime);
+        tr.LookAt(target.position + (target.up * targetOffset));
+    }
+}
