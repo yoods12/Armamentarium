a8f41e6bfec5b78ea6eb4ab9bf7187c4406714e4
유동수 <yoods12@naver.com>
2024-08-13 22:12:51 +0900

    mouse x rotation 만듬
    mouse 좌우 회전 만듬

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class CarController : MonoBehaviour
@@ -15,24 +16,32 @@ public class CarController : MonoBehaviour
     [SerializeField] private Transform frontLeftWheelTransform, frontRightWheelTransform;
     [SerializeField] private Transform rearLeftWheelTransform, rearRightWheelTransform;
 
-    private float horizontalInput, verticalInput;
+    [SerializeField] private Transform bodyGear, coreGear;
+    [SerializeField] private Transform rightWeaponGear, leftWeaponGear;
+
+    private float horizontalInput, verticalInput, mouseInput;
     private float currentSteerAngle, currentbreakForce;
     private bool isBreaking;
-    public Rigidbody rb;
+    Rigidbody rb;
     Transform playerTransform;
+    private float rotSpeed = 200f;
 
 
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
         rb.centerOfMass = Vector3.zero;
+
     }
+
     private void FixedUpdate()
     {
         GetInput();
         HandleMotor();
         HandleSteering();
         UpdateWheels();
+        MouseRotate();
+        UpdateGearLocation();
     }
 
     private void GetInput()
@@ -45,6 +54,9 @@ public class CarController : MonoBehaviour
 
         // Breaking Input
         isBreaking = Input.GetKey(KeyCode.Space);
+
+        // 마우스 좌우 회전
+        mouseInput = Input.GetAxis("Mouse X");
     }
 
     private void HandleMotor()
@@ -86,5 +98,18 @@ public class CarController : MonoBehaviour
 
         wheelTransform.position = Vector3.Lerp(wheelTransform.position, pos, Time.deltaTime * 10f); // 10f는 보간 속도
         wheelTransform.rotation = Quaternion.Slerp(wheelTransform.rotation, rot, Time.deltaTime * 10f);
+    }
+    private void UpdateGearLocation()
+    {
+        bodyGear.localPosition = new Vector3(0, 1.58f, 0);
+        
+        //coreGear.localPosition = new Vector3(0, 0, 0);
+        rightWeaponGear.localPosition = new Vector3(0.41f, 0.41f, 0);
+        leftWeaponGear.localPosition = new Vector3(-0.41f, 0.41f, 0);
+    }
+    private void MouseRotate()
+    {
+        bodyGear.Rotate(Vector3.up * rotSpeed * mouseInput * Time.deltaTime);
+
     }
 }
